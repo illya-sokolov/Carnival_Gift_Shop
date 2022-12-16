2968e3b8bb45bb324e2eb1b94d787e3ec8623623
isokolov <illya.sokolov82@gmail.com>
2022-12-16 11:54:52 +0200

    Stage 1

---
+++ Carnival Gift Shop/task/main.js
@@ -0,0 +1,16 @@
+// write your code here
+console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
+Hello friend! Thank you for visiting the carnival!
+Here's the list of gifts:
+
+Teddy Bear
+Big Red Ball
+Huge Bear
+Candy
+Stuffed Tiger
+Stuffed Dragon
+Skateboard
+Toy Car
+Basketball
+Scary Mask
+`);
